86508045ea0cd0833e7bbc9aca4db83b823eacfb
eddipietro <94018665+eddipietro@users.noreply.github.com>
2022-10-19 11:38:53 -0300

    Intento dos

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ let fyh = new Date().toLocaleString();
 app.get('/fyh', (_req, res) => res.send({ fyh }));
 
 app.get('*', (_req, res) => res.send('Opción no válida!'));
-*/
+----------------------
 
 const http = require("http");
-require('dotenv').config()
+
 
 const server = http.createServer((_req, res)=>{
     res.end("Holllllllllllllllllllllllas ");
@@ -28,4 +28,30 @@ const PORT = process.env.PORT
 
 server.listen(PORT, () => {
     console.log(`Servidor Corriendo en el puerto ${PORT}`)
+})
+*/
+
+//--------------------
+
+const index = require('./index.js');
+
+const express = require('express');
+
+const PORT =process.env.PORT
+
+const app = express();
+const server = app.listen(process.env.PORT || PORT, () => console.log(`Server listening on PORT ${PORT}`));
+server.on('error', err => console.log(`Error: ${err}`));
+
+const productos = new index('productos.txt');
+
+app.get('/productos', async (_req, res) => {
+    const mostrarProductos = await productos.getAll();
+    res.send(mostrarProductos);
+})
+
+app.get('/productoRandom', async (_req, res) => {
+    const p = await productos.getAll();
+    const numeroRandom = Math.floor(Math.random() * p.length);
+    res.send(p[numeroRandom]);
 })
